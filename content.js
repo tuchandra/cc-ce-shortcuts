bdbf43d7aa2abe521b1323d668e2de05b51ad49b
Tushar Chandra <tusharchandra2018@u.northwestern.edu>
2018-11-08 23:39:51 -0600

    Update logic to get left and right answers

--- content.js
+++ content.js
@@ -1,5 +1,3 @@
-var answers = document.getElementsByClassName("ce_answer_link");
-
 var back_key = 73;  // i
 var left_key = 74;  // j
 var right_key = 75;  // k
@@ -8,13 +6,28 @@ if (window == top) {
     window.addEventListener('keyup', doKeyPress, false);
 }
 
+var answers, left_answer, right_answer;
+window.onload = function() {
+    // get the links to the left and right answers -- 
+    // most of the time the left answer is first, but not always;
+    // you can identify it by seeing if it has left:100px.
+    answers = document.getElementsByClassName("ce_answer_link");
+    if (answers[0].firstChild.style.left == '100px') {
+        left_answer = answers[0];
+        right_answer = answers[1];
+    } else {
+        left_answer = answers[1];
+        right_answer = answers[0];
+    }
+}
+
 function doKeyPress(e){
 	if (e.keyCode == left_key) { 
         console.log('left');
-        answers[0].click();
+        left_answer.click();
     } else if (e.keyCode == right_key) {
         console.log('right');
-        answers[1].click();
+        right_answer.click();
     } else if (e.keyCode == back_key) {
         console.log('back');
         document.links[0].click();
